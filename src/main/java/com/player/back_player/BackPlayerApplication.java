f39fa0b575a5d7e84fc90d3460a0bd3ee069890e
Santiago Sosa <ssosag@unal.edu.co>
2025-03-07 01:16:11 -0500

    first commit

---
+++ src/main/java/com/player/back_player/BackPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.player.back_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPlayerApplication.class, args);
+	}
+
+}
