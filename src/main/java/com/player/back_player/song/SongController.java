bb2434fcdcc6d06fc663a778d397a1ac699016a7
Santiago Sosa <ssosag@unal.edu.co>
2025-03-08 14:21:26 -0500

    cors Update song

--- src/main/java/com/player/back_player/song/SongController.java
+++ src/main/java/com/player/back_player/song/SongController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/song")
 public class SongController {
